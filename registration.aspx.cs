78d1f7f6d3fcad949448ce0e5d73fb924346d508
het2809 <hetk93@gmail.com>
2024-12-21 11:37:42 +0530

    Edited Login Button

--- registration.aspx.cs
+++ registration.aspx.cs
@@ -93,8 +93,8 @@ namespace WorkNest
         {
 
 
-            //string connectionString = "Data Source=LAPTOP-C6B669RO\\SQLEXPRESS;Initial Catalog=WORKNEST;Integrated Security=True";
-            string connectionString = "Data Source=VATSAL\\SQLEXPRESS;Initial Catalog=WORKNEST;Integrated Security=True";
+            string connectionString = "Data Source=LAPTOP-C6B669RO\\SQLEXPRESS;Initial Catalog=WORKNEST;Integrated Security=True";
+            //string connectionString = "Data Source=VATSAL\\SQLEXPRESS;Initial Catalog=WORKNEST;Integrated Security=True";
             //string connectionString = "Data Source=DESKTOP-4D8U420\\SQLEXPRESS;Initial Catalog=WORKNEST;Integrated Security=True";
 
             string selectedCity = ddlCity.SelectedItem.Text;
