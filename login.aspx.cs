b39f8b8e53c7a7ef92f9022589a01f4799f6f070
Vatsal106 <vkalathiya106@gmail.com>
2024-12-22 12:42:30 +0530

    Database deleted

--- login.aspx.cs
+++ login.aspx.cs
@@ -13,9 +13,10 @@ namespace WorkNest
 
     public partial class login : System.Web.UI.Page
     {
+        SqlConnection conn;
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+
         }
 
         public void fnConnectDB()
@@ -23,7 +24,7 @@ namespace WorkNest
             try
             {
                 string strconn = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
-                SqlConnection conn;
+
                 conn = new SqlConnection(strconn);
                 if (conn.State != ConnectionState.Open)
                 {
@@ -48,63 +49,60 @@ namespace WorkNest
 
             string username = txtUsername.Text.Trim();
             string password = txtPassword.Text.Trim();
-            string connectionString = "Data Source=VATSAL\\SQLEXPRESS;Initial Catalog=WORKNEST;Integrated Security=True";
-            //string connectionString = "Data Source=LAPTOP-C6B669RO\\SQLEXPRESS;Initial Catalog=WORKNEST;Integrated Security=True";
-            //string connectionString = "Data Source=DESKTOP-4D8U420\\SQLEXPRESS;Initial Catalog=WORKNEST;Integrated Security=True";
+
 
             string loginQuery = "SELECT COUNT(1) FROM REGISTER WHERE USERNAME = @Username AND PASSWORD = @Password";
             string imageQuery = "SELECT IMAGE FROM REGISTER WHERE USERNAME = @Username AND PASSWORD = @Password";
 
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            fnConnectDB();
+            try
             {
-                try
+                conn.Open();
+
+                // Check user credentials
+                SqlCommand loginCmd = new SqlCommand(loginQuery, conn);
+                loginCmd.Parameters.AddWithValue("@Username", username);
+                loginCmd.Parameters.AddWithValue("@Password", password);
+
+                int count = (int)loginCmd.ExecuteScalar();
+
+                if (count == 1)
                 {
-                    conn.Open();
+                    lblMessage.Text = "Login successful!";
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
 
-                    // Check user credentials
-                    SqlCommand loginCmd = new SqlCommand(loginQuery, conn);
-                    loginCmd.Parameters.AddWithValue("@Username", username);
-                    loginCmd.Parameters.AddWithValue("@Password", password);
+                    //// Fetch the user's image
+                    //SqlCommand imageCmd = new SqlCommand(imageQuery, conn);
+                    //imageCmd.Parameters.AddWithValue("@Username", username);
+                    //imageCmd.Parameters.AddWithValue("@Password", password);
 
-                    int count = (int)loginCmd.ExecuteScalar();
-
-                    if (count == 1)
-                    {
-                        lblMessage.Text = "Login successful!";
-                        lblMessage.ForeColor = System.Drawing.Color.Green;
-
-                        //// Fetch the user's image
-                        //SqlCommand imageCmd = new SqlCommand(imageQuery, conn);
-                        //imageCmd.Parameters.AddWithValue("@Username", username);
-                        //imageCmd.Parameters.AddWithValue("@Password", password);
-
-                        //byte[] imageBytes = imageCmd.ExecuteScalar() as byte[];
-                        //if (imageBytes != null && imageBytes.Length > 0)
-                        //{
-                        //    imgPhoto.EnableViewState = true;
-                        //    string base64Image = Convert.ToBase64String(imageBytes);
-                        //    imgPhoto.ImageUrl = "data:image/png;base64," + base64Image;
-                        //}
-                        //else
-                        //{
-                        //    imgPhoto.ImageUrl = "~/Images/default.png"; // Fallback to default image
-                        //}
-                    }
-                    else
-                    {
-                        lblMessage.Text = "Invalid Username or Password!";
-                        lblMessage.ForeColor = System.Drawing.Color.Red;
-                    }
+                    //byte[] imageBytes = imageCmd.ExecuteScalar() as byte[];
+                    //if (imageBytes != null && imageBytes.Length > 0)
+                    //{
+                    //    imgPhoto.EnableViewState = true;
+                    //    string base64Image = Convert.ToBase64String(imageBytes);
+                    //    imgPhoto.ImageUrl = "data:image/png;base64," + base64Image;
+                    //}
+                    //else
+                    //{
+                    //    imgPhoto.ImageUrl = "~/Images/default.png"; // Fallback to default image
+                    //}
                 }
-                catch (Exception ex)
+                else
                 {
-                    Response.Write($"Error: {ex.Message}");
-                }
-                finally
-                {
-                    conn.Close();
+                    lblMessage.Text = "Invalid Username or Password!";
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
                 }
             }
+            catch (Exception ex)
+            {
+                Response.Write($"Error: {ex.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
         }
     }
 }
