4ab94de5cc424f5c9ffd9eef3c5e9e3a4eb4d412
Vatsal106 <vkalathiya106@gmail.com>
2025-02-06 10:29:11 +0530

    Marser page Added

--- Admin/AdminM.Master.cs
+++ Admin/AdminM.Master.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WorkNest.Admin
+namespace WorkNest.Project_Manager
 {
-    public partial class AdminDashboard : System.Web.UI.Page
+    public partial class AdminM : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- Admin/AdminM.Master.designer.cs
+++ Admin/AdminM.Master.designer.cs
@@ -11,7 +11,7 @@ namespace WorkNest.Project_Manager
 {
 
 
-    public partial class HomePage
+    public partial class AdminM
     {
 
         /// <summary>
@@ -22,5 +22,14 @@ namespace WorkNest.Project_Manager
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
