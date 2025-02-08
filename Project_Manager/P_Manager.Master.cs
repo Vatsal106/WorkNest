eacc69a58eec4766e10d494d94f90cf0f570c534
het2809 <hetk93@gmail.com>
2025-02-08 11:32:12 +0530

    init commit

--- Project_Manager/P_Manager.Master.cs
+++ Project_Manager/P_Manager.Master.cs
@@ -11,28 +11,8 @@ namespace WorkNest.Project_Manager
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-                LoadFunctionalities();
-            }
+           
         }
-        public void LoadFunctionalities()
-        {
-            var functionalities = new List<object>
-            {
-                new { Title = "View Assigned Projects", Description = "Overview of all projects assigned to you." },
-                new { Title = "Task Allocation", Description = "Assign tasks to team members and set deadlines." },
-                new { Title = "Monitor Progress", Description = "Track the progress of each project and tasks." },
-                new { Title = "Team Communication", Description = "Interact with team members and stakeholders." },
-                new { Title = "Generate Reports", Description = "Create detailed reports on project status and team performance." },
-                new { Title = "Manage Resources", Description = "Allocate resources effectively to ensure project success." },
-                new { Title = "Approve Leave Requests", Description = "Review and approve leave requests from team members." },
-                new { Title = "Project Budgeting", Description = "Manage project budgets and track expenses." }
-            };
-
-            rptFunctionalities.DataSource = functionalities;
-            rptFunctionalities.DataBind();
-
-        }
+        
     }
 }
