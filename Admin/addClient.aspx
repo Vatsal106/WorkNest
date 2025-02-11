13dc825a6ffdc578b91edcd8e486c0c9a61487dd
het2809 <hetk93@gmail.com>
2025-02-11 09:36:29 +0530

    Added Client Form

---
+++ Admin/addClient.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="addClient.aspx.cs" Inherits="WorkNest.Admin.addClient" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            CLIENT NAME :
+            <asp:TextBox ID="txtClientName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            EMAIL :
+            <asp:TextBox ID="txtEmail" runat="server" TextMode="Email"></asp:TextBox>
+            <br />
+            <br />
+            PHONE NUMBER : <asp:TextBox ID="txtPhoneNumber" runat="server" TextMode="Number"></asp:TextBox>
+            <br />
+            <br />
+            COMPANY NAME : <asp:TextBox ID="txtCompanyName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            ADDRESS :
+            <asp:TextBox ID="txtAddress" runat="server" TextMode="MultiLine"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="SUBMIT" />
+            <br />
+            <br />
+            <asp:Label ID="lblError" class="btn" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Admin/addClient.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WorkNest.Admin
+{
+    public partial class addClient : System.Web.UI.Page
+    {
+        dbConnection dbConn = new dbConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserRole"] == null || Session["UserRole"].ToString() != "Admin")
+            {
+                Response.Redirect("~/AccessDenied.aspx");
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            dbConn.dbConnect();
+            string cName = txtClientName.Text;
+            string email = txtEmail.Text;
+            string phone_number = txtPhoneNumber.Text;
+            string company_name = txtCompanyName.Text;
+            string address = txtAddress.Text;
+            string query = "INSERT INTO CLIENTS(CLIENT_NAME,EMAIL,PHONE_NUMBER,COMPANY_NAME,ADDRESS) VALUES(@NAME,@EMAIL,@NUMBER,@COMPANY_NAME,@ADDRESS)";
+            SqlCommand cmd = new SqlCommand(query,dbConn.con);
+            cmd.Parameters.AddWithValue("@NAME",cName);
+            cmd.Parameters.AddWithValue("@EMAIL", email);
+            cmd.Parameters.AddWithValue("@NUMBER", phone_number);
+            cmd.Parameters.AddWithValue("@COMPANY_NAME", company_name);
+            cmd.Parameters.AddWithValue("@address", address);
+            int rowaffected = cmd.ExecuteNonQuery();
+            if (rowaffected > 0) {
+                lblError.Text = "Client added successfully!";
+                lblError.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblError.Text = "Client Not added!";
+                lblError.ForeColor = Color.Red;
+            }
+        }
+    }
+}
